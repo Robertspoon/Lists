680c797f554216b8971820479163f866d622cf9e
Robertspoon <robertthemcjug@gmail.com>
2023-02-15 09:32:26 -0400

    Listing

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -12,6 +12,41 @@ namespace Lists
         {
             Console.WriteLine("Lists");
 
+            string[] inv1 = new string[8];
+            inv1[0] = ("Health Potion");
+            inv1[1] = ("Shield Potion");
+            inv1[2] = ("");
+            inv1[3] = ("");
+            inv1[4] = ("");
+            inv1[5] = ("");
+            inv1[6] = ("");
+            inv1[7] = ("");
+
+            for(int i = 0; i< inv1.Length; i++)
+            {
+                Console.WriteLine(i + " " + inv1[i]);
+            }
+
+            List<string> inv2 = new List<string>();
+            inv2.Add("Health Potion");
+            inv2.Add("Dabloons");
+            inv2.Add("Sword");
+            inv2.Add("Map");
+
+
+            foreach (string item in inv2)
+            {
+                Console.WriteLine("- " + item);     
+            }
+            Console.WriteLine();
+
+            inv2.Remove("Sword");
+
+            foreach (string item in inv2)
+            {
+                Console.WriteLine("- " + item);
+            }
+
             Console.ReadKey(true);
         }
     }
